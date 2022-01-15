b64e7695f1e7b2eb72ebb311e7cc856bc7b586ba
Mark Pihay <markpy16@gmail.com>
2022-01-15 17:40:21 +0000

    git push

--- verilog/mult32x32_fast_fsm.sv
+++ verilog/mult32x32_fast_fsm.sv
@@ -13,7 +13,7 @@ module mult32x32_fast_fsm (
     output logic clr_prod         // Clear the product register
 );
 
-typedef enum  { start, st1, st2, st3, st4 } state;
+typedef enum  { st0, st1, st2, st3, st4 } state;
 // Put your code here
 // ------------------
 state cur_state;
@@ -21,7 +21,7 @@ state next_state;
 
 always_ff @(posedge clk, posedge reset) begin
     if(reset == 1'b1) begin
-        cur_state <= start;
+        cur_state <= st0;
     end
     else begin
         cur_state <= next_state;
@@ -30,76 +30,111 @@ end
 
 always_comb begin
     next_state = cur_state;
-    if(reset = 1'b1) begin
+    if(reset == 1'b1) begin
+        next_state = st0;
         clr_prod = 1'b1;
         a_sel = 1'b0;
         b_sel = 1'b0;
         shift_sel = 2'b00;
         upd_prod = 1'b0;
         busy = 1'b0;
-    end
-    else begin
+    end else begin
         clr_prod = 1'b0;
+        a_sel = 1'b0;
+        b_sel = 1'b0;
+        shift_sel = 2'b00;
+        upd_prod = 1'b0;
+        busy = 1'b0;
     end
+
     case(cur_state)
-        start: begin
+        st0: begin
             if (start == 1'b1) begin
                 next_state = st1;
-                busy = 1'b1;
+                busy = 1'b0;
                 a_sel = 1'b0;
                 b_sel = 1'b0;
                 shift_sel = 2'b00;
-                upd_prod = 1'b1;
+                upd_prod = 1'b0;
                 clr_prod = 1'b1;
+            end 
+            else if (reset == 1'b1) begin
+                next_state = cur_state;
+                clr_prod = 1'b1;
+            end
+            else begin
+                next_state = cur_state;
+                busy = 1'b0;
+                a_sel = 1'b0;
+                b_sel = 1'b0;
+                shift_sel = 2'b00;
+                upd_prod = 1'b0;
+                clr_prod = 1'b0;
             end
         end
         st1: begin
-            if(b_msw_is_0 == 1'b1)begin
-                next_state = st2;
-            end
-            else begin
-                next_state = st2;
-                a_sel = 1'b0;
-                b_sel = 1'b1;
-                shift_sel = 2'b01;
-                upd_prod = 1'b1;
-                clr_prod = 1'b0;
-            end
-         
-        end
-        st2: begin
-        if(a_msw_is_0 == 1'b0) begin
+        if ((a_msw_is_0 == 1'b1) &&(b_msw_is_0 == 1'b1)) begin
+            next_state = st0;                
+            a_sel = 1'b0;
+            b_sel = 1'b0;
+            shift_sel = 2'b00;
+            busy = 1'b1;
+            upd_prod = 1'b1;
+            clr_prod = 1'b0;
+        end else if(b_msw_is_0 == 1'b1) begin
             next_state = st4;
-        end
-        else begin
+            a_sel = 1'b1;
+            b_sel = 1'b0;
+            shift_sel = 2'b01;
+            busy = 1'b1;
+            upd_prod = 1'b1;
+            clr_prod = 1'b0;
+        end else if(a_msw_is_0 == 1'b1) begin
             next_state = st3;
+            a_sel = 1'b0;
+            b_sel = 1'b1;
+            shift_sel = 2'b01;
+            busy = 1'b1;
+            upd_prod = 1'b1;
+            clr_prod = 1'b0;
+        end else begin
+            next_state = st2;
+            a_sel = 1'b1;
+            b_sel = 1'b1;
+            shift_sel = 2'b10;
+            busy = 1'b1;
+            upd_prod = 1'b1;
+            clr_prod = 1'b0;
+        end
+        end
+
+        st2: begin
+            next_state = st3;
+            a_sel = 1'b0;
+            b_sel = 1'b1;
+            shift_sel = 2'b01;
+        end
+
+        st3:begin
+            next_state = st4;
             a_sel = 1'b1;
             b_sel = 1'b0;
             shift_sel = 2'b01;
         end
-        end
-        st3:begin
-        if (b_msw_is_0 == 1'b1)begin
-            next_state = st4;
-        end
-        else begin
-            next_state = st4;
-            a_sel = 1'b1;
-            b_sel = 1'b1;
-            shift_sel = 2'b10;
-        end
-        end
+
         st4:begin
-            next_state = start;
+            next_state = st0;
             a_sel = 1'b0;
             b_sel = 1'b0;
             shift_sel = 2'b00;
-            upd_prod = 1'b0;
-            busy = 1'b0;
         end
-        default:
+        
     endcase
+
 end
+
+// Put your code here
+// ------------------
 
 // End of your code
 
